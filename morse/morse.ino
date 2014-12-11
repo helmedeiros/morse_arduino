3a7d06f895ddc854e311eaa85f75af638afc5ffe
Helio Medeiros <helio.cabralmedeiros@gmail.com>
2014-12-10 22:16:23 -0200

    refactore to use a single port set

--- morse/morse.ino
+++ morse/morse.ino
@@ -9,26 +9,27 @@
 
   This example code is in the public domain.
  */
-
+const int ACTIVE_PORT = 13;
 
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin 13 as an output.
-  pinMode(13, OUTPUT);
+  pinMode(ACTIVE_PORT, OUTPUT);
 }
 
-void on(){
-  digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+void on(int port){
+  digitalWrite(port, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);              // wait for a second
 }
 
-void off(){
-  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
+void off(int port){
+  digitalWrite(port, LOW);    // turn the LED off by making the voltage LOW
   delay(1000);              // wait for a second
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  on();
-  off();
+  on(ACTIVE_PORT);
+  off(ACTIVE_PORT);
+  off(ACTIVE_PORT);
 }
