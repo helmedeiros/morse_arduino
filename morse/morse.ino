b031d8b08c8d5ef5672affb2ed4203c22d743f48
Helio Medeiros <helio.cabralmedeiros@gmail.com>
2014-12-10 22:10:17 -0200

    extract methods on and off

--- morse/morse.ino
+++ morse/morse.ino
@@ -17,10 +17,18 @@ void setup() {
   pinMode(13, OUTPUT);
 }
 
-// the loop function runs over and over again forever
-void loop() {
+void on(){
   digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);              // wait for a second
+}
+
+void off(){
   digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
   delay(1000);              // wait for a second
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  on();
+  off();
 }
